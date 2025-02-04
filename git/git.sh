834bd94d49efef9b22fe566e2fc4519afceddc4c
Yuta Fujiyama <yuta.technology@gmail.com>
2025-02-05 00:28:19 +0900

    fix git scripts

--- git/git.sh
+++ git/git.sh
@@ -3,8 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" && pwd)"
 source $SCRIPT_DIR/../setup_utils/utils.sh
 
-SOURCE_FILES="
-"
+SOURCE_FILES=""
 
 CHECK_FILES="
     $HOME/.gitconfig
@@ -22,15 +21,16 @@ fi
 
 COMMAND="git"
 
-is_configure_completed "$SOURCE_FILES" "$SYMLINKS" "$COMMAND"
+is_configure_completed "$SOURCE_FILES" "$CHECK_FILES" "$COMMAND"
 CONFIG_COMPETED=$?
 
 if [ $CONFIG_COMPETED -eq 1 ]; then
     # gitの設定が完了していない場合は、gitコマンドを上書き
-    source $SCRIPT_DIR/scripts/overwrite_git_command.sh
+    source $SCRIPT_DIR/source/overwrite_git_command.sh
 else
     source_all "$SOURCE_FILES"
-    source $SCRIPT_DIR/git_env.sh
+    source $SCRIPT_DIR/source/git_env.sh
 fi
 
 source $SCRIPT_DIR/../setup_utils/unset_utils.sh
+source $SCRIPT_DIR/../setup_utils/unset_vars.sh
--- git/scripts/setup_git.sh
+++ git/scripts/setup_git.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" && pwd)"
+source $SCRIPT_DIR/../../setup_utils/utils.sh
 
-# gitがインストールされているか確認
-if ! command -v git > /dev/null 2>&1; then
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOSの場合
-    echo "Setting up git on macOS..."
-    brew install git
+COMMANDS="git"
+check_and_install_commands "$COMMANDS"
 
-  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Ubuntuの場合
-    echo "Installing git on Ubuntu..."
-    sudo apt install git -y
-
-  else
-    echo "Unsupported OS. This script supports macOS and Ubuntu only."
-    exit 0
-  fi
-fi
-
-echo "download_git_completion"
 $SCRIPT_DIR/download_git_completion.sh
-echo "create_config_symlink"
 $SCRIPT_DIR/create_config_symlink.sh

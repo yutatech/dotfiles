01120a40d373b76ba83dec968a15f6c74ba9a55f
Yuta Fujiyama <yuta.technology@gmail.com>
2025-02-05 01:04:13 +0900

    tmux configuration

---
+++ tmux/scripts/download_tmux_completion.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" && pwd)"
+
+mkdir -p $SCRIPT_DIR/../completion
+
+curl -o $SCRIPT_DIR/../completion/tmux.completion.bash \
+  https://raw.githubusercontent.com/Bash-it/bash-it/refs/heads/master/completion/available/tmux.completion.bash

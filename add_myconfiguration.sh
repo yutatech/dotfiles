7af2b37cf47591495c1630aef48e3ae4fbde96e0
Yuta Fujiyama <yuta.technology@gmail.com>
2025-02-12 18:15:31 +0900

    update add_myconfiguration

--- add_myconfiguration.sh
+++ add_myconfiguration.sh
@@ -2,11 +2,13 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" && pwd)"
 
-if [ -n "${BASH_VERSION}" ]; then
+parent_shell=$(ps -o comm= -p "$PPID")
+
+if [ $parent_shell = bash ]; then
     # bashの場合
   source $SCRIPT_DIR/setup_utils/add_line_to_bashrc.sh
   add_line_to_bashrc "source $SCRIPT_DIR/my-configuration.bash"
-elif [ -n "${ZSH_VERSION}" ]; then
+elif [ $parent_shell = zsh ]; then
     # zshの場合
   source $SCRIPT_DIR/setup_utils/add_line_to_zshrc.sh
   add_line_to_zshrc "source $SCRIPT_DIR/my-configuration.zsh"

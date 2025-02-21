3cd9f8ac5d62693928054322448561210400d23f
Yuta Fujiyama <yuta.technology@gmail.com>
2025-02-22 01:27:00 +0900

    ros2 configuration

--- ros2/source/ros2_env.sh
+++ ros2/source/ros2_env.sh
@@ -1,5 +1,3 @@
-# ROS 2 configuration
-source /opt/ros/humble/setup.bash
 export RCUTILS_COLORIZED_OUTPUT=1
 export RCUTILS_CONSOLE_OUTPUT_FORMAT=[{severity}][{time}][{name}]:\ {message}
 export ROSCONSOLE_FORMAT=[${severity}][${time}][${node}:${logger}]: ${message}

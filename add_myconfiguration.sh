3ffd0278c716335d2167a08522bd835ee1a5531b
Yuta Fujiyama <yuta.technology@gmail.com>
2025-02-27 18:51:37 +0900

    .xxshrcがなければ自動で作る

--- add_myconfiguration.sh
+++ add_myconfiguration.sh
@@ -2,14 +2,20 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" && pwd)"
 
-parent_shell=$(ps -o comm= -p "$PPID")
+parent_shell=$(ps -o comm= -p "$PPID" | awk -F'/' '{print $NF}')
 
 if [ $parent_shell = bash ]; then
-    # bashの場合
+  # bashの場合
+  if [ ! -e "$HOME/.bashrc" ]; then
+    touch ~/.bashrc
+  fi
   source $SCRIPT_DIR/setup_utils/add_line_to_bashrc.sh
   add_line_to_bashrc "source $SCRIPT_DIR/my-configuration.bash"
 elif [ $parent_shell = zsh ]; then
-    # zshの場合
+  # zshの場合
+  if [ ! -e "$HOME/.zshrc" ]; then
+    touch ~/.zshrc
+  fi
   source $SCRIPT_DIR/setup_utils/add_line_to_zshrc.sh
   add_line_to_zshrc "source $SCRIPT_DIR/my-configuration.zsh"
 fi

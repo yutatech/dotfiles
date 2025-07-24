1e46a6a9372ee612405ae42ce5df7fc309ee2aeb
Yuta Fujiyama <yuta.technology@gmail.com>
2025-07-24 18:36:06 +0900

    update git completion

--- git/git_comp.sh
+++ git/git_comp.sh
@@ -1,24 +1,10 @@
 if [ -n "${ZSH_VERSION}" ]; then
-    _git_custom_commands() {
-        local context state line
-        declare -A opt_args
+    typeset -gA _git_third_party_commands  # グローバル連想配列として宣言（既存なら無視される）
 
-        _arguments -C \
-            '(-): :->command'
+    _git_third_party_commands[clear]=":delete all merged branches"
+    _git_third_party_commands[fixup]=":fix up a commit"
 
-        case $state in
-            command)
-            local -a custom_cmds
-            custom_cmds=(
-                'clear:delete all merged branches'
-                'fixup:fix up a commit'
-            )
-            _describe -t command 'git command' custom_cmds
-            ;;
-        esac
-        _git
-    }
-    compdef _git_custom_commands git
+    compdef _git git
 
     _git-fixup() {
         __git_recent_commits

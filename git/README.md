6dcf1f1dead7b5f14e3b94a1dd0c4d885b869825
Yuta Fujiyama <yuta.technology@gmail.com>
2025-07-24 16:00:34 +0900

    add git fixup
    update

---
+++ git/README.md
@@ -0,0 +1,5 @@
+# git
+
+## サブコマンドの作り方
+1. `git/cmd/git-cmd`にコマンドの中身を作成
+2. `git/git_comp.sh`に作成したコマンドのcompletionを追記
--- git/git_comp.sh
+++ git/git_comp.sh
@@ -3,14 +3,24 @@ if [ -n "${ZSH_VERSION}" ]; then
         _git
 
         local -a _descriptions _values
-        _descriptions=('clear -- delete all merged branches')
-        _values=('clear')
+        _descriptions=('clear -- delete all merged branches',
+                       'fixup -- fix up a commit')
+        _values=('clear' 'fixup')
         compadd -d _descriptions -a _values
     }
     compdef _git_custom_commands git
+
+    _git-fixup() {
+        __git_recent_commits
+    }
+    compdef _git-fixup git-fixup
 elif [ -n "${BASH_VERSION}" ]; then
     _git_clear() {
         COMPREPLY=()
     }
     complete -F _git_clear git-clear
+    _git_fixup() {
+        COMPREPLY=()
+    }
+    complete -F _git_fixup git-fixup
 fi

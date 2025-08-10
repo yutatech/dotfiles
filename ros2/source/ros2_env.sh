8877ae5c7c6a596b67240cb65398fe513165d3fe
Yuta Fujiyama <yuta.technology@gmail.com>
2025-08-11 00:22:20 +0900

    fix ros2 setup script and add sourceros2

--- ros2/source/ros2_env.sh
+++ ros2/source/ros2_env.sh
@@ -1,3 +1,9 @@
 export RCUTILS_COLORIZED_OUTPUT=1
 export RCUTILS_CONSOLE_OUTPUT_FORMAT=[{severity}][{time}][{name}]:\ {message}
 export ROSCONSOLE_FORMAT=[${severity}][${time}][${node}:${logger}]: ${message}
+
+
+if [ -n "${ZSH_VERSION}" ]; then
+    export PATH="$REPO_DIR/ros2/cmd:$PATH"
+    fpath=($REPO_DIR/ros2/comp $fpath)
+fi

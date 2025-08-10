5cf2daf8ab08abe62fc091e9676a01141f231a1b
Yuta Fujiyama <yuta.technology@gmail.com>
2025-08-11 01:10:27 +0900

    implement docker setup script

---
+++ docker/docker.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# docker configuration
+source $REPO_DIR/setup_utils/utils.sh
+
+SOURCE_FILES=""
+
+CHECK_FILES=""
+
+SYMLINKS=""
+
+COMMANDS="docker"
+
+is_configure_completed "$SOURCE_FILES $CHECK_FILES" "$SYMLINKS" "$COMMANDS"
+CONFIG_COMPETED=$?
+
+if [ $CONFIG_COMPETED -eq 1 ]; then
+    # 設定が完了していない場合は、コマンドを上書き
+    source $REPO_DIR/docker/source/overwrite_command.sh
+else
+    source_all "$SOURCE_FILES"
+fi
+
+source $REPO_DIR/setup_utils/unset_utils.sh

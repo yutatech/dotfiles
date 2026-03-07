4162b931ddec8b0e63b2bacd5e43a85615ebbdda
Yuta Fujiyama <yuta.technology@gmail.com>
2026-03-07 19:55:31 +0900

    bugfix

--- chrome/source/overwrite_command.sh
+++ chrome/source/overwrite_command.sh
@@ -2,8 +2,8 @@ chrome() {
   echo "chrome configuration is not completed. Would you like to configure it now? (y/n)"
   read answer
   if [ "$answer" = "y" ]; then
-    local SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-    local REPO_DIR=$(cd $SCRIPT_DIR && git rev-parse --show-toplevel 2>/dev/null)
+    local SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" && pwd)"
+    local REPO_DIR="$(cd "$SCRIPT_DIR" && git rev-parse --show-toplevel 2>/dev/null)"
     $REPO_DIR/chrome/scripts/setup_chrome.sh
     unset -f chrome 2>/dev/null
     source $REPO_DIR/chrome/chrome.sh
